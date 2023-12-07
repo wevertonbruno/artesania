8805bf8e838616c51b7be283cc0685af6cb9feec
Weverton Bruno <weverton.ssantos@mercadolivre.com>
2023-12-06 22:40:53 -0300

    feat: Add tv to button

--- src/components/button/icon.tsx
+++
@@ -1,5 +0,0 @@
-import { ElementType } from "react";
-
-export function Icon({ icon: Icon }: { icon: ElementType }) {
-  return <Icon size={20} />;
-}
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,7 +1,28 @@
-import { Container } from "./container";
-import { Icon } from "./icon";
+import { ButtonHTMLAttributes, ComponentProps } from "react";
+import { VariantProps, tv } from "tailwind-variants";
 
-export const Button = {
-  Container: Container,
-  Icon: Icon,
-};
+const buttonClass = tv({
+  base: "flex items-center justify-center rounded-md bg-zinc-100 text-sm font-medium text-zinc-900 hover:bg-zinc-200",
+  variants: {
+    size: {
+      default: "px-4 py-2",
+      sm: "px-3 py-1.5 text-sm",
+      xs: "px-2 py-1 text-xs",
+    },
+    circle: {
+      true: "rounded-full",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+    circle: false,
+  },
+});
+
+interface ButtonProps
+  extends ComponentProps<"button">,
+    VariantProps<typeof buttonClass> {}
+
+export function Button({ circle, size, ...props }: ButtonProps) {
+  return <button className={buttonClass({ circle, size })} {...props} />;
+}
